6076b3eaa78e24db0c30c2114e084df80041b296
FabioDiCeglie <dcfabio96@gmail.com>
2022-02-23 11:54:44 +0100

    set homepage component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Loading from "./components/Loading";
 import MessageBox from "./components/MessageBox";
 import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
+import Homepage from "./pages/Homepage/Homepage";
 
 import { useDispatch, useSelector } from "react-redux";
 import { selectAppLoading } from "./store/appState/selectors";
@@ -33,15 +34,16 @@ function App() {
   }, [dispatch]);
 
   return (
-    <div className='App'>
+    <div className="App">
       <Navigation />
       <MessageBox />
       {isLoading ? <Loading /> : null}
       <Routes>
-        <Route exact path='/' element={<Home />} />
-        <Route path='/other' element={<Other />} />
-        <Route path='/signup' element={<SignUp />} />
-        <Route path='/login' element={<Login />} />
+        <Route exact path="/homepage" element={<Home />} />
+        <Route path="/" element={<Homepage />} />
+        <Route path="/other" element={<Other />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );
---
+++ src/store/Homepage/reducer.js
@@ -0,0 +1,10 @@
+const initialState = {
+  spaces: null,
+};
+export default function spacesReducer(state = initialState, action) {
+  switch (action.type) {
+    default: {
+      return state;
+    }
+  }
+}
